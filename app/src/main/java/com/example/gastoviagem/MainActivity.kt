8952f5c81640e72a0e264cde18709bccef813bde
gabogSb <gbarcellos1701@gmail.com>
2023-07-17 23:02:17 -0300

    first project in Kotlin

--- app/src/main/java/com/example/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/example/gastoviagem/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.gastoviagem
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.example.gastoviagem.databinding.ActivityMainBinding
 import org.w3c.dom.Text
 
@@ -25,7 +26,31 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 		}
 	}
 
-	private fun calculate() {}
+	private fun isValid(): Boolean {
+		return (binding.editDistance.text.toString() != ""
+				&& binding.editPrice.text.toString() != ""
+				&& binding.editAutonomy.text.toString() != ""
+				&& binding.editAutonomy.text.toString().toFloat() != 0f
+				&& binding.editPrice.text.toString().toFloat() != 0f
+				&& binding.editDistance.text.toString().toFloat() != 0f
+				)
+	}
+
+	private fun calculate() {
+
+		if (isValid()) {
+			val distance = binding.editDistance.text.toString().toFloat()
+			val price = binding.editPrice.text.toString().toFloat()
+			val autonomy = binding.editAutonomy.text.toString().toFloat()
+
+			val totalValue = (distance * price) / autonomy
+
+			binding.textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+		} else {
+			Toast.makeText(this, R.string.validation_fill_all_fields, Toast.LENGTH_SHORT).show()
+		}
+
+	}
 
 
 }
